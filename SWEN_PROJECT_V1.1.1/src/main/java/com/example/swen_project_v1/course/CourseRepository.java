0f8f6186632b63c89daed430eadc0740de3aa015
Saood Al Jarman <ska7474@g.rit.edu>
2026-03-09 21:37:30 +0400

    update

--- SWEN_PROJECT_V1.1.1/src/main/java/com/example/swen_project_v1/course/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.swen_project_v1.course;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-    Optional<Course> findByCode(String code);
-    boolean existsByCode(String code);
-}
--- SWEN_PROJECT_V1.1.1/src/main/java/com/example/swen_project_v1/course/DayOfWeek.java
+++
@@ -1,5 +0,0 @@
-package com.example.swen_project_v1.course;
-
-public enum DayOfWeek {
-    MON, TUE, WED, THU, FRI
-}
